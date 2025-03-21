a0c6f578880e5b6e35544cbe025ae793f829f69c
bvvn1 <bvvn@protonmail.com>
2025-03-21 19:01:24 +0200

    Some updates

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,6 +4,7 @@ extends Node
 var score = 0
 
 
+
 func add_point():
 	score += 1
 
--- scripts/gogo.gd
+++ scripts/gogo.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 400.0
+const JUMP_VELOCITY = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,8 +10,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,3 +22,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+play(name: gogo)
