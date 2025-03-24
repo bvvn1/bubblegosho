f9be0bf46b0ff70caadd6c85f07e2d0b3e8d1cd9
bvvn1 <bvvn@protonmail.com>
2025-03-24 22:48:26 +0200

    Added eating sounds
    by removing child node (sound) and putting into the main scene

--- scripts/duner.gd
+++ scripts/duner.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
-@onready var sound = $AudioStreamPlayer2D
 
 func _physics_process(delta):
 	position.y += 700 * delta
 
 func _on_body_entered(body: Node2D) -> void:
 	GameManager.add_point()
-	GameManager.playsound()
-	queue_free()  # Remove item after pickup
-	
- 
+	var sound = $eating_sound  # Get reference to the sound node
+	remove_child(sound)  # Detach from current node
+	get_tree().current_scene.add_child(sound)  # Reparent to the main scene
+	sound.play()  # Play the sound
+	queue_free()  # Remove the object immediately
+	 
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -8,12 +8,6 @@ var score = 0
 func add_point():
 	score += 1
 
-var audio_player: AudioStreamPlayer
-
-func playsound():
-	audio_player.play()
-	
-
 func _ready():
 	var timer = Timer.new()
 	timer.wait_time = randf_range(2.5, 4)
